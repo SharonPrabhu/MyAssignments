b26db6d90de5de0b3e20d7e32e9719c79ff2a4ed
SharonPrabhu <sharonprabhu21@gmail.com>
2026-05-16 00:55:57 +0530

    Add files via upload
    Week1 assignments

---
+++ Week1_assignments/Day1/letkeyword.js
@@ -0,0 +1,9 @@
+let browserVersion = "chrome";
+function getBrowserVersion(){
+{
+    if(browserVersion === "Firefox")
+    let browserVersion="Edge"
+}
+console.log(browserVersion);
+}
+getBrowserVersion();
---
+++ Week1_assignments/Day1/varkeyword.js
@@ -0,0 +1,38 @@
+const browserVersion = "chrome";
+function getBrowserVersion(){
+{
+    if(browserVersion === "chrome")
+var browserVersion = "Edge";
+}
+console.log(browserVersion);
+}
+getBrowserVersion();
+
+/*
+
+//let
+
+let browserName = "chrome";
+function getBrowserName(){
+{
+    if(browserName === "chrome")
+let browserName = "Firefox";
+}
+console.log(browserName);
+}
+getBrowserName();
+
+//const
+
+const browserId = "chrome";
+function getBrowserId(){
+{
+    if(browserId === "chrome")
+const browserId = "Safari";
+}
+console.log(browserId);
+}
+getBrowserId();
+//
+
+*/
